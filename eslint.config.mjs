b5f1251a5d1130dadcc9982b4c800bba37018c55
monzurul002 <monzurul002@gmail.com>
2024-06-07 18:20:38 +0600

    project set up

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'no-undef': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+    },
+  },
+  {
+    ignores: ['**/.env/', '**/node_modules/', '**/dist/'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+  },
+);

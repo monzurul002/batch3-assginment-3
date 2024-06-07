cf493bc229915f1bc6368440b665426acf7dd195
monzurul002 <monzurul002@gmail.com>
2024-06-07 20:24:10 +0600

    product schema created

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { Request, Response } from 'express';
-
+import cors from 'cors';
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World! Hello bangladesh An my dfjd');
---
+++ src/modules/products/product.model.ts
@@ -0,0 +1,61 @@
+import { Schema, model } from 'mongoose';
+import { TInventory, TProduct, TProductVariant } from './product.interface';
+
+//productVariant schema
+const ProductVariantSchema = new Schema<TProductVariant>({
+  type: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+});
+//inventory schema
+
+const InventorySchema = new Schema<TInventory>({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+//product schema
+const ProductSchema = new Schema<TProduct>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  variants: {
+    type: [ProductVariantSchema],
+    required: true,
+  },
+  inventory: {
+    type: InventorySchema,
+    required: true,
+  },
+});
+
+// product schema model
+export const ProductModel = model('Product', ProductSchema);
